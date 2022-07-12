f966cfb5ede0b9c28e217a8432579580752425d6
jeziorny <jakub.jeziorny@gmail.com>
2022-07-12 20:33:29 +0200

    poprawki w copy

--- 01/bmi.py
+++ 01/bmi.py
@@ -1,9 +1,8 @@
 # BMI
 
 h = float(input("Podaj wzrost w metrach z kropeczką: "))
-w = float(input("Podaj wagę w kg:"))
+w = float(input("Podaj wagę w kg: "))
 
 bmi = round(w / (h ** 2), 2)
-
 
 print("Twoje BMI to: " + str(bmi))
